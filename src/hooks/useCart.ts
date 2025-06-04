1c425008402f9f3ded6ed69d0219a69b1f376b4e
Jefferson Nunes <96424928+jeffynunes09@users.noreply.github.com>
2025-06-04 20:49:20 -0300

    Update useCart.ts

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,4 +1,4 @@
 import { useContext } from "react";
-import { CartContext } from "../context/CartContext";
+import { CartContext } from "../context/cartContext";
 export const useCart = () => useContext(CartContext);
 
